48a1e1bc5b5fd9c1d539822866389dc1d89253fa
KalingaYapa <kkyapa@gmail.com>
2024-02-17 22:37:40 +0800

    initial commit

---
+++ src/main/java/com/project/sltourguide/SltourguideApplication.java
@@ -0,0 +1,13 @@
+package com.project.sltourguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SltourguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SltourguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sltourguide/SltourguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sltourguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SltourguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
